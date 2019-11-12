4c428b1889c6066fae4c631a191ce4757a327716
AbdelIdir <homeworkoracle@protonmail.com>
2019-11-12 18:03:41 +0100

    Added more styling : padding-top for the navbar

--- apple-nav/src/Nav.js
+++ apple-nav/src/Nav.js
@@ -2,9 +2,18 @@ import React from "react";
 import styled from "styled-components";
 import { Route, Link, NavLink } from "react-router-dom";
 import { SubNav } from "./SubNav";
+
 export const Nav = () => {
+  const NavWrapperDiv = styled.div`
+    padding-top: 80px;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  `;
+
   return (
-    <>
+    <NavWrapperDiv>
       <NavLink to="/">HOME </NavLink>
       <NavLink to="/Mac">MAC</NavLink>
       <NavLink to="/Ipad">IPAD</NavLink>
@@ -15,6 +24,6 @@ export const Nav = () => {
       <NavLink to="/Support">SUPPORT</NavLink>
       <NavLink to="/Search">SEARCH</NavLink>
       <NavLink to="/Cart">CART</NavLink>
-    </>
+    </NavWrapperDiv>
   );
 };
