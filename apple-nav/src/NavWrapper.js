c6857192369002104de08ebf5a319a6df7949b58
AbdelIdir <homeworkoracle@protonmail.com>
2019-11-12 14:35:17 +0100

    Made a Subnav,Nav,Navwrapper components,and styling the Navwrapper with styled-components

---
+++ apple-nav/src/NavWrapper.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from "styled-components";
+
+
+export const NavWrapper = () => {
+
+const Header = styled.header`
+
+width: 100%;
+height: 200px;
+background:blue;
+color:red;
+
+`
+return(
+
+<Header>
+
+
+    Hey hey hey hi 
+</Header>
+
+)
+}
